84759fe8b9222217417915154bdf297c8f6cab75
devs-sourav <souravacharjee361@gmail.com>
2024-01-23 17:03:08 +0600

    node module

--- backend/controllers/registrationController.js
+++ backend/controllers/registrationController.js
@@ -3,15 +3,16 @@ const bcrypt = require('bcrypt');
 const nodemailer = require("nodemailer");
 const otpGenerator = require('otp-generator')
 
+
 let registrationController = async (req,res) =>{
 
     let {name,email,password} = req.body
 
     
-let otp = otpGenerator.generate(6, { upperCaseAlphabets: false,lowerCaseAlphabets: false, specialChars: false });
+    let otp = otpGenerator.generate(6, { upperCaseAlphabets: false,lowerCaseAlphabets: false, specialChars: false });
 
     let existingUser = await User.find({email:email})
-
+ 
     if(existingUser.length == 0){
         if(!email){
             res.send("Please enter a valid email")
@@ -20,7 +21,8 @@ let otp = otpGenerator.generate(6, { upperCaseAlphabets: false,lowerCaseAlphabet
         }else if(!password){
             res.send("Please enter a valid email")
         }else{
-            bcrypt.hash(password, 10,async function(err, hash) {
+
+            bcrypt.hash(password, 10, async function(err, hash) {
                 // Store hash in your password DB.
                 let user = new User({
                     name:name,
@@ -28,9 +30,8 @@ let otp = otpGenerator.generate(6, { upperCaseAlphabets: false,lowerCaseAlphabet
                     password:hash,
                     otp:otp
                 })
-            
+                
                 user.save()
-
 
                 const transporter = nodemailer.createTransport({
                     service:"gmail",
@@ -41,12 +42,14 @@ let otp = otpGenerator.generate(6, { upperCaseAlphabets: false,lowerCaseAlphabet
                     },
                 });
 
+
                 const info = await transporter.sendMail({
                     from: 'souravacharjee360@gmail.com', // sender address
                     to: email, // list of receivers
                     subject: "Verify Your Account", // Subject line
-                    html: `<div style="display: flex;width: 600px;height: 200px;"> <div style="width: 50%;height: 100px;">Please Verify your email by click on this button <a href="https://www.figma.com/">Verify</a>${otp}</div></div>`,// html body
+                    html: `<table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse:separate;mso-table-lspace:0;mso-table-rspace:0;background-color:#fff;border:1px solid #eaebed;border-radius:16px;width:100%"><tr><td style="font-family:Helvetica,sans-serif;font-size:16px;vertical-align:top;box-sizing:border-box;padding:24px"><p style="font-family:Helvetica,sans-serif;font-size:16px;font-weight:400;margin:0;margin-bottom:16px">Ecommerce</p><p style="font-family:Helvetica,sans-serif;font-size:16px;font-weight:400;margin:0;margin-bottom:16px">Here OTP is : </p><h2>${otp}</h2><br><p style="font-family:Helvetica,sans-serif;font-size:16px;font-weight:400;margin:0;margin-bottom:16px">Click here for verification :</p><table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse:separate;mso-table-lspace:0;mso-table-rspace:0;box-sizing:border-box;width:100%;min-width:100%"><tr><td align="left" style="font-family:Helvetica,sans-serif;font-size:16px;vertical-align:top;padding-bottom:16px"><table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse:separate;mso-table-lspace:0;mso-table-rspace:0;width:auto"><tr><td style="font-family:Helvetica,sans-serif;font-size:16px;vertical-align:top;border-radius:4px;text-align:center;background-color:#0867ec" valign="top" align="center" bgcolor="#0867ec"><a href="https://www.w3schools.com/nodejs/nodejs_email.asp" target="_blank" style="border:solid 2px #0867ec;border-radius:4px;box-sizing:border-box;cursor:pointer;display:inline-block;font-size:16px;font-weight:700;margin:0;padding:12px 24px;text-decoration:none;text-transform:capitalize;background-color:#0867ec;border-color:#0867ec;color:#fff">Verify Your Email</a></td></tr></table></td></tr></table><p style="font-family:Helvetica,sans-serif;font-size:16px;font-weight:400;margin:0;margin-bottom:16px">Don't share it. It's can use once</p></td></tr></table>`,// html body
                 });
+
             });
     
 
@@ -56,9 +59,6 @@ let otp = otpGenerator.generate(6, { upperCaseAlphabets: false,lowerCaseAlphabet
     }else{
         res.send("Email Already Exist.")
     }
- 
-    
-
 
 }
 
